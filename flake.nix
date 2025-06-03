eb7e43b0a937db84590b9c9585e810ab72d5fc95
Mutsuha Asada <me@momee.mt>
2025-06-03 19:19:26 +0900

    init: note.momee.mt

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = with inputs; [
+        treefmt-nix.flakeModule
+      ];
+
+      systems = import inputs.systems;
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nil
+            mdbook
+          ];
+        };
+
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs = {
+            alejandra.enable = true;
+            mdformat.enable = true;
+          };
+        };
+      };
+    };
+}
---
+++ src/SUMMARY.md
@@ -0,0 +1,6 @@
+# Summary
+
+- [note.momee.mt](./README.md)
+- [信号処理]()
+    - [フーリエ変換](./signal-processing/fourier.md)
+
